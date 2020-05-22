0459339165c732c26a3b367fe57be24f6ad3546a
EvertonBorges <evertonsoaresborges@gmail.com>
2020-05-22 15:38:59 -0400

    feat - implement test class with TDD methodology and insert a file with 80000+ words

--- app/src/androidTest/java/br/com/cesar/searchonalist/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/br/com/cesar/searchonalist/ExampleInstrumentedTest.java
@@ -2,26 +2,66 @@ package br.com.cesar.searchonalist;
 
 import android.content.Context;
 
-import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import java.util.ArrayList;
+import java.util.List;
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
+import static org.junit.Assert.assertEquals;
+
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+    private static final List<String> words = new ArrayList<>();
+
+    @BeforeClass
+    public static void populateWords() {
+        words.clear();
+
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+//        font: https://docs.oracle.com/javase/tutorial/collections/interfaces/examples/dictionary.txt
+        final List<String> wordsInDocument = Utils.getWordsInDocument(appContext, "dictionary.txt");
+        words.addAll(wordsInDocument);
+    }
+
     @Test
     public void useAppContext() {
-        // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
         assertEquals("br.com.cesar.searchonalist", appContext.getPackageName());
     }
+
+    @Test
+    public void testFindWordYou() {
+        List<String> wordsFiltered = new ArrayList<>();
+        final String wordToFilter = "you";
+        for (String word : words) {
+            final boolean isPartialPermutationXorTypo =
+                    Utils.isPartialPermutationXorTypo(wordToFilter, word);
+
+            if (isPartialPermutationXorTypo) wordsFiltered.add(word);
+        }
+
+        assertEquals(24, wordsFiltered.size());
+    }
+
+    @Test
+    public void testFindWordGlove() {
+        List<String> wordsFiltered = new ArrayList<>();
+        final String wordToFilter = "glove";
+        for (String word : words) {
+            final boolean isPartialPermutationXorTypo =
+                    Utils.isPartialPermutationXorTypo(wordToFilter, word);
+
+            if (isPartialPermutationXorTypo) wordsFiltered.add(word);
+        }
+
+        assertEquals(8, wordsFiltered.size());
+    }
+
 }
